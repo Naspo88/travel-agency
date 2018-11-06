5134adb19730659cf65d9c3231105484f8c0290b
Roberto Minghi <robertominghi@msn.com>
2018-11-06 16:39:52 +0000

    lesson 13

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssvars = require('postcss-simple-vars');
 const nest = require('postcss-nested');
+const cssimport = require('postcss-import');
 
 gulp.task('default', function () {
     gulp.start('watch');
@@ -15,7 +16,7 @@ gulp.task('html', function () {
 
 gulp.task('css', function () {
     return gulp.src('./app/assets/css/**/*.css')
-        .pipe(postcss([nest, cssvars, autoprefixer]))
+        .pipe(postcss([cssimport, nest, cssvars, autoprefixer]))
         .pipe(gulp.dest('./app/temp/css'));
 });
 
