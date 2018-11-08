ebd65cafafd251af8b41705a5931f23f6f727fe2
Roberto Minghi <robertominghi@msn.com>
2018-11-08 13:15:09 +0000

    reusable blocks

--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -4,9 +4,14 @@ const autoprefixer = require('autoprefixer');
 const cssvars = require('postcss-simple-vars');
 const nest = require('postcss-nested');
 const cssimport = require('postcss-import');
+const mixins = require('postcss-mixins');
 
 gulp.task('css', function () {
     return gulp.src('./app/assets/css/style.css')
-        .pipe(postcss([cssimport, cssvars, nest, autoprefixer]))
+        .pipe(postcss([cssimport, mixins, cssvars, nest, autoprefixer]))
+        .on('error', function (errorInfo) {
+            console.log(errorInfo.toString());
+            this.emit('end');
+        })
         .pipe(gulp.dest('./app/temp/css'));
 });
