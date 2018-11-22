840ccef73db39c925cb3ae7bfe8c8855201b3a5f
Roberto Minghi <robertominghi@msn.com>
2018-11-22 15:05:35 +0000

    header for large screens

--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -5,10 +5,11 @@ const cssvars = require('postcss-simple-vars');
 const nest = require('postcss-nested');
 const cssimport = require('postcss-import');
 const mixins = require('postcss-mixins');
+const hexrgba = require('postcss-hexrgba');
 
 gulp.task('css', function () {
     return gulp.src('./app/assets/css/style.css')
-        .pipe(postcss([cssimport, mixins, cssvars, nest, autoprefixer]))
+        .pipe(postcss([cssimport, mixins, cssvars, nest, hexrgba, autoprefixer]))
         .on('error', function (errorInfo) {
             console.log(errorInfo.toString());
             this.emit('end');
